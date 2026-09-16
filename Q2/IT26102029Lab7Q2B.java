4ad800e83778bbf6189e55435a4220bb62fb826a
IT26102029 <it26102029@my.sliit.lk>
2026-09-16 06:25:43 +0530

    Add files via upload

---
+++ Q2/IT26102029Lab7Q2B.java
@@ -0,0 +1,18 @@
+public class IT26102029Lab7Q2B {
+public static void main(String[] args) {
+
+
+    for (int row = 1; row <= 5; row++) {
+
+        System.out.print(row + " - ");
+
+        for (int star = 1; star <= row; star++) {
+            System.out.print("* ");
+        }
+
+        System.out.println();
+    }
+}
+
+
+}
---
+++ Q2/IT26102029Lab7Q2C.java
@@ -0,0 +1,16 @@
+public class IT26102029Lab7Q2C {
+public static void main(String[] args) {
+
+
+    for (int row = 5; row >= 1; row--) {
+
+        for (int column = 1; column <= row; column++) {
+            System.out.print(row);
+        }
+
+        System.out.println();
+    }
+}
+
+
+}
